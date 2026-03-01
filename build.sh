dbe432616c38184dc24630613d823038c80716ae
AfzalRana <edmedpublic@gmail.com>
2026-03-01 08:46:03 +0500

    Update build.sh to install portaudio

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 set -o errexit
+set -o pipefail
+set -o nounset
 
+# 1. Install system dependencies for PyAudio
+apt-get update && apt-get install -y portaudio19-dev
+
+# 2. Install Python dependencies
 pip install -r requirements.txt
 
+# 3. Collect static files
 python manage.py collectstatic --noinput
+
+# 4. Apply migrations
 python manage.py migrate
