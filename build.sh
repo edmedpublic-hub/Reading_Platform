6257f82e5f590d28de662ae8ca3e04214f49bc74
AfzalRana <edmedpublic@gmail.com>
2026-03-01 14:28:25 +0500

    Add automatic superuser creation

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 # exit on error
 set -o errexit
 
-# 1. Install Python dependencies from your updated requirements.txt
+# 1. Install Python dependencies
 pip install -r requirements.txt
 
-# 2. Collect static files (Required for CSS/JS to show up on the web)
+# 2. Collect static files
 python manage.py collectstatic --no-input
 
-# 3. Apply database migrations to SQLite
+# 3. Apply database migrations
 python manage.py migrate
+
+# 4. Create admin user automatically (FREE PLAN WORKAROUND)
+python manage.py createsuperuser --noinput || true
